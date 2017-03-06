77e87ffb9107f68a110b03aefef97e7bc541adcd
duneding <maj.dagostino@gmail.com>
2017-03-05 21:13:28 -0300

    Initial commit.

---
+++ iris.py
@@ -0,0 +1,114 @@
+# Load libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+# Summarize the Dataset
+# Dimensions of Dataset - shape
+print(dataset.shape)
+
+# Peek at the Data - head
+print(dataset.head(20))
+
+# Statistical Summary - descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('class').size())
+
+# Data Visualization
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+#plt.show()
+
+# Univariate Plots - histograms
+dataset.hist()
+#plt.show()
+
+# Multivariate Plots - scatter plot matrix
+scatter_matrix(dataset)
+#plt.show()
+
+# Evaluate Some Algorithms
+# 1-Separate out a validation dataset.
+# 2-Set-up the test harness to use 10-fold cross validation.
+# 3-Build 5 different models to predict species from flower measurements
+# 4-Select the best model.
+
+# Create a Validation Dataset
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test Harness
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+# Build Models
+'''
+    Logistic Regression (LR)
+    Linear Discriminant Analysis (LDA)
+    K-Nearest Neighbors (KNN).
+    Classification and Regression Trees (CART).
+    Gaussian Naive Bayes (NB).
+    Support Vector Machines (SVM).
+'''
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# Select Best Model
+# Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+# Make Predictions
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
