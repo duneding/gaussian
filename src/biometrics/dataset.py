1b6687666581786fd8e64547cba305bacab6469f
duneding <maj.dagostino@gmail.com>
2017-06-16 20:54:14 -0300

    Add comparision Kmeans VS Mini Batch KMeans.

--- src/biometrics/dataset.py
+++ src/biometrics/dataset.py
@@ -3,21 +3,33 @@ import csv
 
 PAGINATION = 50
 SEARCH_SIZE = 50
-CAP = 20000
+CAP = 100000
 TYPE = 0
 TIME = 1
 KEY = 2
 LOC = 3
 REP = 4
 
-request={'size': SEARCH_SIZE, 'from': 0, 'query': {'match': {
-    'tracked_data.form_params.department': '7daf94ed9226aa945a58b6550d142558'}}}
+query = {"bool": {"must": [
+            {"range": {"database_id": {"gte": 2536268}}},
+            {'match': {'tracked_data.form_params.department': '7daf94ed9226aa945a58b6550d142558'}}
+        ]}}
+request = {'size': SEARCH_SIZE, 'from': 0, 'query': query}
 
 w = csv.writer(open('output.csv', 'w'))
 
 
 def keyboard(track):
-    return track['_source']['tracked_data']['keyboard']
+    events = track['_source']['tracked_data']['keyboard']
+    return events[0]['events'] if len(events) > 0 else []
+
+
+def device(track):
+    return track['_source']['tracked_data']['device']
+
+
+def meli_params(track):
+    return track['_source']['tracked_data']['meli_params']
 
 
 def has_hits(tracks):
@@ -27,6 +39,7 @@ def has_hits(tracks):
 def is_same_event(previous, current):
     return  (previous is None) or \
             ((previous[TYPE] == current[TYPE]) and (previous[KEY] == current[KEY]))
+
 
 def summarize_events(events):
     prev_event = None
@@ -66,45 +79,71 @@ def is_left(event):
     return event[LOC] == 1
 
 
+def is_up(event):
+    return event[TYPE] == 0
+
+
+def is_down(event):
+    return event[TYPE] == 1
+
+
+def is_last(index, events_count):
+    return (index + 1) == events_count
+
+
 '''
 Return: TupleAVG(pressure, dwell_time, flight_time)
 '''
 
 
 def calculate_features(events):
-    sum_flight_time, sum_delta_time, sum_flight_time, sum_pressure = 0, 0, 0, 0
-    alt_right_count, alt_left_count, shift_right_count, shift_left_count = 0, 0, 0, 0
-    enter_count, backspace_count, space_count = 0, 0, 0
+    sum_delta_time, sum_flight_time, sum_dwell_time, sum_flight_time, sum_pressure = 0, 0, 0, 0, 0
+    alt_right_count, alt_left_count, shift_right_count, shift_left_count, tab_count = 0, 0, 0, 0, 0
+    ctrl_right_count, ctrl_left_count, enter_count, backspace_count, space_count = 0, 0, 0, 0, 0
 
     events = summarize_events(events)
     keys_count = len(filter(lambda x: x[0] == 1, events[1]))
     start_time = events[0]
+    events_count = len(events[1])
 
-    for event in events[1]:
+    for index, event in enumerate(events[1]):
 
         # Stats
-        sum_delta_time += event[TIME]
         sum_pressure += event[REP]
-        sum_flight_time += event[TIME] if event[TYPE] else 0
+        sum_delta_time += event[TIME]
+        sum_dwell_time += event[TIME] if is_up(event) else 0
+        sum_flight_time += event[TIME] if is_down(event) or (not is_last(index, events_count)) else 0
 
         # Keys
         alt_right_count += 1 if is_key_code(event, 18) and is_right(event) else 0
         alt_left_count += 1 if is_key_code(event, 18) and is_left(event) else 0
         shift_right_count += 1 if is_key_code(event, 16) and is_right(event) else 0
         shift_left_count += 1 if is_key_code(event, 16) and is_left(event) else 0
+        ctrl_right_count += 1 if is_key_code(event, 17) and is_right(event) else 0
+        ctrl_left_count += 1 if is_key_code(event, 17) and is_left(event) else 0
+        tab_count += 1 if is_key_code(event, 9) else 0
         enter_count += 1 if is_key_code(event, 13) else 0
         backspace_count += 1 if is_key_code(event, 8) else 0
         space_count += 1 if is_key_code(event, 32) else 0
 
     avg_pressure = round(sum_pressure/float(keys_count), 5) if keys_count > 0 else 0
-    avg_dwell_time = round(sum_delta_time/float(keys_count), 5) if keys_count > 0 else 0
+    avg_dwell_time = round(sum_dwell_time/float(keys_count), 5) if keys_count > 0 else 0
     avg_flight_time = round(sum_flight_time/float(keys_count), 5) if keys_count > 0 else 0
     error_rate = round(backspace_count/float(keys_count), 5) if keys_count > 0 else 0
-    end_time = start_time + sum_delta_time
+    avg_alt_right = round(alt_right_count/float(keys_count), 5) if keys_count > 0 else 0
+    avg_alt_left = round(alt_left_count/float(keys_count), 5) if keys_count > 0 else 0
+    avg_shift_right = round(shift_right_count/float(keys_count), 5) if keys_count > 0 else 0
+    avg_shift_left = round(shift_left_count/float(keys_count), 5) if keys_count > 0 else 0
+    avg_ctrl_right = round(ctrl_right_count/float(keys_count), 5) if keys_count > 0 else 0
+    avg_ctrl_left = round(ctrl_left_count/float(keys_count), 5) if keys_count > 0 else 0
+    avg_enter = round(enter_count/float(keys_count), 5) if keys_count > 0 else 0
+    avg_space = round(space_count/float(keys_count), 5) if keys_count > 0 else 0
+    avg_tab = round(tab_count/float(keys_count), 5) if keys_count > 0 else 0
+    end_time = start_time + sum_dwell_time
 
     return start_time, end_time, avg_pressure, avg_dwell_time, avg_flight_time,\
-           alt_right_count, alt_left_count, shift_right_count, shift_left_count,\
-           enter_count, backspace_count, space_count, error_rate
+           avg_alt_right, avg_alt_left, avg_shift_right, avg_shift_left, \
+           avg_ctrl_right, avg_ctrl_left, avg_enter, avg_space, avg_tab, error_rate
 
 
 def search():
@@ -122,39 +161,71 @@ def get_hits(tracks):
 
 def get_tracks(results):
     hits = get_hits(results)
-    return [keyboard(track) for track in hits if len(keyboard(track)) > 0]
+    return [{'meli_params': meli_params(track), 'device': device(track), 'keyboard': keyboard(track)} for track in hits if len(keyboard(track)) > 0]
+
+
+def match_device_with(feature, value):
+    return int(value in str(feature).lower())
 
 
 # Processing data
 results = search()
-while has_hits(results) and request['from'] < CAP:
+while has_hits(results) and (request['size'] + request['from'] <= CAP):
 
     tracks = get_tracks(results)
     for track in tracks:
-        key_events = track[0]['events']
+        keyboard_events = track['keyboard']
+
+        device_app_version = track['device']['appVersion']
+        device_platform = track['device']['platform']
 
         # Calculate Features:
-        pattern = ''.join([str(event[0]) for event in key_events])
+        pattern = ''.join([str(event[0]) for event in keyboard_events])
+        is_android = match_device_with(device_app_version, 'android')
+        is_iphone = match_device_with(device_platform, 'iphone')
+        is_ipad = match_device_with(device_platform, 'ipad')
+        is_ipod = match_device_with(device_platform, 'ipod')
+        is_mac = match_device_with(device_platform, 'macintel')
+        is_linux = match_device_with(device_platform, 'linux') & ~match_device_with(device_app_version, 'android')
+        is_window = match_device_with(device_platform, 'win')
+        is_blackberry = match_device_with(device_platform, 'blackberry')
+        is_playstation = match_device_with(device_platform, 'playstation')
+        is_arm = match_device_with(device_platform, 'arm')
+        is_masking_agent = match_device_with(device_platform, 'masking-agent')
+        is_freebsd = match_device_with(device_platform, 'freebsd')
+        is_osmeta = match_device_with(device_platform, 'osmeta')
+        is_anomaly_user_agent = match_device_with(device_platform, 'android') & match_device_with(device_app_version, 'macintosh')
+        is_empty_platform = 1 if not device_platform else 0
+        user_id = str(track['meli_params']['user_id'])
+        user_id = user_id if user_id != 'unknown' else -1
+
         (start_time,
          end_time,
          avg_dwell_time,
          avg_pressure,
          avg_flight_time,
-         alt_right_count,
-         alt_left_count,
-         shift_right_count,
-         shift_left_count,
-         enter_count,
-         backspace_count,
-         space_count,
-         error_rate) = calculate_features(key_events)
+         avg_alt_right,
+         avg_alt_left,
+         avg_shift_right,
+         avg_shift_left,
+         avg_ctrl_right,
+         avg_ctrl_left,
+         avg_enter,
+         avg_space,
+         avg_tab,
+         error_rate) = calculate_features(keyboard_events)
 
         # Write on file
-        w.writerow([start_time, end_time, avg_pressure, avg_dwell_time, avg_flight_time,
-                    alt_right_count, alt_left_count, shift_right_count, shift_left_count,
-                    enter_count, backspace_count, space_count, error_rate, pattern])
+        instance = [start_time, end_time, avg_pressure, avg_dwell_time, avg_flight_time,
+                    avg_alt_right, avg_alt_left, avg_shift_right, avg_shift_left,
+                    avg_ctrl_right, avg_ctrl_left, avg_enter, avg_space,
+                    avg_tab, error_rate, is_android, is_iphone, is_ipad, is_ipod, is_mac,
+                    is_linux, is_window, is_blackberry, is_playstation, is_arm,
+                    is_masking_agent, is_freebsd, is_osmeta, is_anomaly_user_agent,
+                    is_empty_platform, user_id, pattern]
+        w.writerow(instance)
 
     update_pagination()
-    tracks = search()
+    results = search()
 
 print 1
--- src/biometrics/model.py
+++ src/biometrics/model.py
@@ -1,46 +1,266 @@
-import scipy.cluster.hierarchy as hcluster
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.decomposition import PCA
 import pandas as pd
+from datetime import datetime
+from matplotlib import pyplot as plt
+from matplotlib import cm as cm
 import seaborn as sns
-from pandas.tools.plotting import scatter_matrix
-import numpy
-from numpy import inf
+import time
+import numpy as np
+from sklearn.metrics.pairwise import pairwise_distances_argmin
 
-def plot_scatter(x, y, clusters):
-    # plotting
-    plt.scatter(x, y, c=clusters, edgecolors='g')
-    plt.axis("equal")
-    title = "threshold: %f, number of clusters: %d" % (thresh, len(set(clusters)))
-    plt.title(title)
+# Constant
+DATASET_PATH = "output.csv"
+NUM_CLUSTERS = 4
+MAX_ITERATIONS = 10
+INITIALIZE_CLUSTERS = ['k-means++', 'random']
+CONVERGENCE_TOLERANCE = 0.001
+NUM_THREADS = 10
+BATCH_SIZE = 1000
+SAMPLE_SIZE = 100000
+COLORS = ['red', 'blue', 'green', 'yellow', 'gray', 'pink', 'violet', 'brown',
+          'cyan', 'magenta']
+FEATURES = ['start_time', 'end_time', 'avg_pressure', 'avg_dwell_time', 'avg_flight_time',
+            'avg_alt_right', 'avg_alt_left', 'avg_shift_right', 'avg_shift_left',
+            'avg_ctrl_right', 'avg_ctrl_left', 'avg_enter', 'avg_space', 'avg_tab',
+            'error_rate', 'is_android', 'is_iphone', 'is_ipad', 'is_ipod', 'is_mac',
+            'is_linux', 'is_window', 'is_blackberry', 'is_playstation', 'is_arm',
+            'is_masking_agent', 'is_freebsd', 'is_osmeta', 'is_anomaly_user_agent',
+            'is_empty_platform', 'user_id', 'pattern']
+
+
+def get_dataframe_from(dataset):
+    return pd.read_csv(dataset, names=FEATURES)
+
+
+def normalize(data):
+    return pd.DataFrame((data - data.mean()) / (data.max() - data.min())).fillna(value=-1)
+
+
+def correlation_matrix(df):
+    fig = plt.figure(figsize=[9,9])
+    ax1 = fig.add_subplot(111)
+    cmap = cm.get_cmap('jet', 30)
+    cax = ax1.imshow(df.corr(), interpolation="nearest", cmap=cmap)
+    ax1.grid(True)
+    plt.title('Biometric Feature Correlation')
+    features = filter(lambda x: x != 'pattern' or x != 'user_id', FEATURES)
+    ax1.set_xticklabels(features,fontsize=6)
+    ax1.set_yticklabels(features,fontsize=6)
+    fig.colorbar(cax, ticks=[0,0.05,0.1,0.15,0.2,0.25,0.30,0.35,0.40,0.45,0.50,.55,.6,.65,.7,.75,.8,.85,.90,.95,1])
     plt.show()
 
-tracks = pd.read_csv('output.csv', names=['start_time', 'end_time', 'avg_pressure', 'avg_dwell_time', 'avg_flight_time', 'pattern'])
-tracks.pop('pattern')
-tracks_norm = (tracks - tracks.mean()) / (tracks.max() - tracks.min())
-tracks = numpy.log10(tracks_norm)
-tracks[tracks == -inf] = 0
-tracks_norm = tracks
 
-# clustering
-thresh = 3
-clusters = hcluster.fclusterdata(tracks_norm, thresh, criterion="distance")
-plot_scatter(tracks_norm.end_time, tracks_norm.avg_pressure, clusters)
-plot_scatter(tracks_norm.end_time, tracks_norm.start_time, clusters)
-plot_scatter(tracks_norm.avg_pressure, tracks_norm.avg_dwell_time, clusters)
-plot_scatter(tracks_norm.avg_dwell_time, tracks_norm.avg_flight_time, clusters)
+def correlation_heatmap(df):
+    sns.set(context="paper", font="monospace")
+    corrmat = df.corr()
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(12, 9))
+
+    # Draw the heatmap using seaborn
+    sns.heatmap(corrmat, vmax=.8, square=True)
+    features = filter(lambda x: x != 'pattern' or x != 'user_id', FEATURES)
+    ax.set_xticklabels(features,fontsize=6, rotation=90)
+    ax.set_yticklabels(features,fontsize=6, rotation=0)
+    f.tight_layout()
+
+
+def print_results(centroids, num_cluster_points):
+    print '\n\nFINAL RESULT:'
+    for i, c in enumerate(centroids):
+        print '\tCluster %d' % (i + 1)
+        print '\t\tNumber Points in Cluster %d' % num_cluster_points.count(i)
+        print '\t\tCentroid: %s' % str(centroids[i])
+
+
+def plot_results(centroids, num_cluster_points, points):
+    plt.plot()
+    for nc in range(len(centroids)):
+        # plot points
+        points_in_cluster = [boolP == nc for boolP in num_cluster_points]
+        for i, p in enumerate(points_in_cluster):
+            if bool(p):
+                plt.plot(points[i][0], points[i][1], linestyle='None',
+                         color=COLORS[nc], marker='.')
+        # plot centroids
+        centroid = centroids[nc]
+        plt.plot(centroid[0], centroid[1], 'o', markerfacecolor=COLORS[nc],
+                 markeredgecolor='k', markersize=10)
+    plt.show()
+
+
+def dimension_reduction(data):
+    pca = PCA(n_components=2).fit(data)
+    # 2-dimensions
+    return pca.transform(data)
+
+
+def fit_mbk_means():
+    #MBKMeans
+    mbk_means = MiniBatchKMeans(init=INITIALIZE_CLUSTERS[0], n_clusters=NUM_CLUSTERS, batch_size=BATCH_SIZE,
+                                n_init=10, max_no_improvement=10, verbose=0)
+    time_start = time.time()
+    mbk_means.fit(data)
+    time_end = time.time() - time_start
+
+    return mbk_means, time_end
+
+
+def fit_k_means():
+    #KMeans
+    k_means = KMeans(init=INITIALIZE_CLUSTERS[0], n_clusters=NUM_CLUSTERS, n_init=10)
+
+    time_start = time.time()
+    k_means.fit(data)
+    time_end = time.time() - time_start
+
+    return k_means, time_end
+
+def get_cluster_centers(k_means, mbk_means):
+    return (np.sort(k_means.cluster_centers_, axis=0),
+            np.sort(mbk_means.cluster_centers_, axis=0))
+
+
+def get_labels(data, k_means_cluster_centers, mbk_means_cluster_centers):
+    return (pairwise_distances_argmin(data, k_means_cluster_centers),
+            pairwise_distances_argmin(data, mbk_means_cluster_centers))
+
+
+def initialize_plot_params():
+    fig = plt.figure(figsize=(8, 3))
+    fig.subplots_adjust(left=0.02, right=0.98, bottom=0.05, top=0.9)
+    colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+    return fig, colors, 'train time: %.2fs\ninertia: %f'
+
+
+def plot_diff_clustering(mbk_means_labels, k_means_labels, data, order, fig):
+    # Initialise the different array to all False
+    different = (mbk_means_labels == 4)
+    ax = fig.add_subplot(1, 3, 3)
+
+    for k in range(NUM_CLUSTERS):
+        different += ((k_means_labels == k) != (mbk_means_labels == order[k]))
+
+    identic = np.logical_not(different)
+    ax.plot(data[identic, 0], data[identic, 1], 'w',
+            markerfacecolor='#bbbbbb', marker='.')
+    ax.plot(data[different, 0], data[different, 1], 'w',
+            markerfacecolor='m', marker='.')
+    ax.set_title('Difference')
+    ax.set_xticks(())
+    ax.set_yticks(())
+
+def plot_clustering(labels, centers, title, order, fig, colors, axes):
+
+    ax = fig.add_subplot(axes[0], axes[1], axes[2])
+    for k, col in zip(range(NUM_CLUSTERS), colors):
+
+        if order is None:
+            my_members = labels == k
+        else:
+            my_members = labels == order[k]
+
+        cluster_center = centers[k]
+        ax.plot(data[my_members, 0], data[my_members, 1], 'w',
+                markerfacecolor=col, marker='.')
+        ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+                markeredgecolor='k', markersize=6)
+
+    ax.set_title(title)
+    ax.set_xticks(())
+    ax.set_yticks(())
+
+def clustering(data):
+
+    #MBKMeans
+    (mbk_means, time_end_mbk) = fit_mbk_means()
+
+    #KMeans
+    (k_means, time_end_km) = fit_k_means()
+
+    # The same colors for the same cluster
+    (k_means_cluster_centers, mbk_means_cluster_centers) = get_cluster_centers(k_means, mbk_means)
+    (k_means_labels, mbk_means_labels) = get_labels(data, k_means_cluster_centers, mbk_means_cluster_centers)
+    order = pairwise_distances_argmin(k_means_cluster_centers,
+                                  mbk_means_cluster_centers)
+
+
+    return k_means, mbk_means, time_end_km, time_end_mbk, k_means_labels, mbk_means_labels,\
+           k_means_cluster_centers, mbk_means_cluster_centers, order
+
+
+def k_means(data, num_clusters, max_iterations, init_cluster, tolerance,
+            num_threads):
+
+    # Object KMeans
+    kmeans = KMeans(n_clusters=num_clusters, max_iter=max_iterations,
+                    init=init_cluster, tol=tolerance, n_jobs=num_threads)
+
+    # Training
+    start = datetime.now()
+    kmeans.fit(data)
+    print 'End: ' + str(datetime.now()-start)
+
+    # Obtain centroids and number Cluster of each point
+    centroids = kmeans.cluster_centers_
+    num_cluster_points = kmeans.labels_.tolist()
+
+    # Print final result
+    print_results(centroids, num_cluster_points)
+
+    # Plot Final results
+    plot_results(centroids, num_cluster_points, dimension_reduction(data.as_matrix()))
+
+
+# Visualize correlations
+#correlation_matrix(df)
+#correlation_heatmap(df)
+
+# Clustering
+#k_means(data_norm, NUM_CLUSTERS, MAX_ITERATIONS, INITIALIZE_CLUSTERS[0],
+#            CONVERGENCE_TOLERANCE, NUM_THREADS)
+
+# Prepare data
+df = get_dataframe_from(DATASET_PATH)
+df.sort(['user_id'])
+
+# Normalize
+data = normalize(df.ix[:SAMPLE_SIZE, 0:30])
+
+# Training
+(
+    k_means,
+    mbk_means,
+    time_end_km,
+    time_end_mbk,
+    k_means_labels,
+    mbk_means_labels,
+    k_means_cluster_centers,
+    mbk_means_cluster_centers,
+    order
+) = clustering(data)
+
+# Dimension Reduction
+data = dimension_reduction(data.as_matrix())
+
+# Initialize Plot params
+(figure, colors, stats) = initialize_plot_params()
+
+# Plot KMeans
+k_means_axes = (1, 3, 1)
+plot_clustering(k_means_labels, k_means_cluster_centers, 'kmeans', None, figure, colors, k_means_axes)
+plt.text(0.5, 1,  stats % (
+    time_end_km, k_means.inertia_))
+
+# Plot MBKMeans
+mbk_means_axes = (1, 3, 2)
+plot_clustering(mbk_means_labels, mbk_means_cluster_centers, 'mbk_means', order, figure, colors, mbk_means_axes)
+plt.text(0.5, 1,  stats % (
+    time_end_mbk, mbk_means.inertia_))
+
+# Plot Differences between both algorithms
+plot_diff_clustering(mbk_means_labels, k_means_labels, data, order, figure)
+plt.show()
 
 print 1
-
-# do unsupervised clustering
-# =============================================
-'''
-estimator = KMeans(n_clusters=3, random_state=0)
-X = stacked.values.reshape(len(stacked), 1)
-cluster = estimator.fit_predict(X)
-
-# check the mean value of each cluster
-X[cluster==0].mean()  # Out[53]: 324.73175293698534
-X[cluster==1].mean()  # Out[54]: 6320.8504071851467
-X[cluster==2].mean()  # Out[55]: 1831.1473140192766
-'''
